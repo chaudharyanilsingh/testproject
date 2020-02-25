4ee6cb88c1913cd0137affd51ee56ff86531d502
unknown <anilchaudhary8739@gmail.com>
2020-02-25 13:35:24 +0530

    test

---
+++ src/main/java/company/TestingApplication.java
@@ -0,0 +1,15 @@
+package company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(TestingApplication.class, args);
+	
+	}
+
+}
